5744db9349d41bcb17104789a46410495889c666
Adedero <118962395+Adedero@users.noreply.github.com>
2024-05-12 14:21:06 +0100

    Changed database to MongoDM Atlas

--- database/database.js
+++ database/database.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const mongoose = require('mongoose')
 
-const database = mongoose.connect(process.env.MONGODB_URI)
+const database = mongoose.connect(process.env.MONGODB_URI_PRODUCTION)
 .then(() => console.log('Database connection established'))
 .catch((err) => console.error(err))
 
